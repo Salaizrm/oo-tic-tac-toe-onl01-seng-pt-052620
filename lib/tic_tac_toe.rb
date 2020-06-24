120d59af3f10a4a543b8976ffc2c27ebb360d78b
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-24 00:16:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,53 +9,53 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
   ]
-  
+
   attr_reader :board
-  
+
   def initialize
     @board = []
-    i = 0 
-    while i < 9 
+    i = 0
+    while i < 9
       @board[i] = " "
       i += 1
     end
     @board = Array.new(9," ")
     @board = [" "," "," "," "," "," "," "," "," "]
   end
-  
-  def display_board 
+
+  def display_board
     puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
     puts ("-----------")
     puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
     puts ("-----------")
     puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
   end
-  
+
   def input_to_index(input)
     @index = input.to_i - 1
   end
-  
+
   def move(index, token)
     board[index] = token
   end
-  
+
   def position_taken?(index)
     board[index] == "X" || board[index] == "O"
   end
-  
+
   def valid_move?(index)
     !position_taken?(index) && index.between?(0,8)
   end
-  
-  def turn_count 
+
+  def turn_count
     board.count { |space| space == "X" || space == "O"}
   end
-  
-  def current_player 
+
+  def current_player
     turn_count.even? ? "X" : "O"
   end
-  
-  def turn 
+
+  def turn
     # ask for input
     puts "Please make a move by entering a number between 1-9"
     # get input
@@ -80,7 +80,7 @@ class TicTacToe
     # index = -1
     # until valid_move?(index)
     #   puts "Please make a move by entering a number between 1-9"
-    #   input = gets.strip 
+    #   input = gets.strip
     #   index = input_to_index(input)
     #   if valid_move?(index)
     #     move(index, current_player)
