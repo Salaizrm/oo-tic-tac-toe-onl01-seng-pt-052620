ea8b2cf302b16121541603ddb7b15025cfa22787
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-24 00:16:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,6 +74,9 @@ class TicTacToe
     else
       turn
     end
+
+    def won?
+    end
     # index = -1
     # until valid_move?(index)
     #   puts "Please make a move by entering a number between 1-9"
